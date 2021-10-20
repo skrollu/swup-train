37291bc5bb673d4f379cb311ee86bdf36ef5c885
Mathieu Ibersien <mat98-76@live.fr>
2021-10-20 22:25:39 +0200

    Les fichiers test, pour le plugin head qui semble bien fonctionner, permet d'avoir plusieurs feuilles de style

--- head/test.js
+++ head/test.js
@@ -3,6 +3,7 @@ const options = {
         'a[href^="' +
         window.location.origin +
         '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+    plugins: [new SwupHeadPlugin()]
 };
 
 const swup = new Swup (options);
